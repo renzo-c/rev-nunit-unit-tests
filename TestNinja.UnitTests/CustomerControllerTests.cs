0060dec738c28dfeaf0bdcb6524d5d0c7a949752
Renzo Navarro <rcnavarrop@gmail.com>
2022-08-12 08:00:20 -0400

    Adds tests to validate the return type of methods in controller

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController _customerController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _customerController = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var result = _customerController.GetCustomer(0);
+
+            // Not Found (exact object)
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            // Not Found (or one of its derivatives)
+            Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var result = _customerController.GetCustomer(8);
+
+            // Not Found (exact object)
+            Assert.That(result, Is.TypeOf<Ok>());
+
+            // Not Found (or one of its derivatives)
+            Assert.That(result, Is.InstanceOf<Ok>());
+        }
+    }
+}
