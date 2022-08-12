5ccc3d9260a07bbbad7cfe847f641761766c574b
Renzo Navarro <rcnavarrop@gmail.com>
2022-08-11 23:30:47 -0400

    Adds HtmlFormatterTests (Specificity and generality of test cases)

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+            var result = formatter.FormatAsBold("abc");
+
+            // Too specific
+            // Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+            // Too general
+            // Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+
+            // A better one
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
