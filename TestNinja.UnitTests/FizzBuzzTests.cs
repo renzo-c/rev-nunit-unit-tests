8994ed573bd69da9c046080f6403a6b3d665cde8
Renzo Navarro <rcnavarrop@gmail.com>
2022-08-18 23:29:59 -0400

    Implements tests for fizzbuzz function

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,37 @@
+﻿using TestNinja.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3and5_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3Only_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5Only_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnNumber()
+        {
+            var result = FizzBuzz.GetOutput(13);
+            Assert.That(result, Is.EqualTo("13"));
+        }
+    }
+}
